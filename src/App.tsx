b452303f0e24c00237ba755a5c3a11c0c839b015
Matheus Najjar <matheusnajjar@gmail.com>
2024-07-20 20:48:09 -0700

    feat: adds tsx structure to the page

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,17 @@ import Form from './components/Form';
 
 function App() {
   return (
-    <>
+    <div>
       <h1>Subscribe</h1>
-      <Form />
-    </>
+      <p>Sign up for our newsletter and stay tuned</p>
+      <div>
+        <Form />
+      </div>
+      <p>
+        When you sign up, you will receive our emails with the best tips, offers
+        and news.
+      </p>
+    </div>
   );
 }
 
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,24 @@
 const Form = () => {
-  return <div>Form</div>;
+  return (
+    <form>
+      <div>
+        <label>Name</label>
+        <input type="text" placeholder="Insert your name" />
+      </div>
+      <div>
+        <label>E-mail</label>
+        <input type="email" placeholder="Insert your best e-mail" />
+      </div>
+      <div>
+        <a href="#">Read the terms</a>
+        <div>
+          <input id="checkbox" type="checkbox" />
+          <label htmlFor="checkbox">Agree with terms</label>
+        </div>
+      </div>
+      <button>Register</button>
+    </form>
+  );
 };
 
 export default Form;
