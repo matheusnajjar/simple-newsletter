b92050e1be8e0543d39a9ab473c99d84c8913590
Matheus Najjar <matheusnajjar@gmail.com>
2024-07-20 21:10:24 -0700

    style: finishes tailwind for the page

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import Form from './components/Form';
 
 function App() {
   return (
-    <div>
-      <h1>Subscribe</h1>
-      <p>Sign up for our newsletter and stay tuned</p>
-      <div>
+    <div className="bg-slate-900 min-h-screen w-full flex flex-col items-center justify-center">
+      <h1 className="font-bold text-[2rem] text-white">Subscribe</h1>
+      <p className="text-white">Sign up for our newsletter and stay tuned</p>
+      <div className="w-96 mt-4 bg-stone-200 px-4 py-5 rounded-lg">
         <Form />
       </div>
-      <p>
+      <p className="text-slate-100 text-xs w-96 mt-2 text-center">
         When you sign up, you will receive our emails with the best tips, offers
         and news.
       </p>
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,22 +1,43 @@
 const Form = () => {
   return (
-    <form>
-      <div>
-        <label>Name</label>
-        <input type="text" placeholder="Insert your name" />
+    <form className="flex flex-col gap-3">
+      <div className="flex flex-col">
+        <label className="text-sm" htmlFor="name">
+          Name
+        </label>
+        <input
+          type="text"
+          placeholder="Insert your name"
+          className="rounded-lg py-2 px-2 text-sm placeholder:text-sm placeholder:text-stone-400"
+        />
       </div>
-      <div>
-        <label>E-mail</label>
-        <input type="email" placeholder="Insert your best e-mail" />
+      <div className="flex flex-col">
+        <label className="text-sm" htmlFor="email">
+          E-mail
+        </label>
+        <input
+          type="email"
+          placeholder="Insert your best e-mail"
+          className="rounded-lg py-2 px-2 text-sm placeholder:text-sm placeholder:text-stone-400"
+        />
       </div>
-      <div>
-        <a href="#">Read the terms</a>
-        <div>
-          <input id="checkbox" type="checkbox" />
-          <label htmlFor="checkbox">Agree with terms</label>
+      <div className="flex flex-col">
+        <a href="#" className="text-xs underline mb-2">
+          Read the terms
+        </a>
+        <div className="flex gap-1 items-center">
+          <input id="agree" type="checkbox" />
+          <label htmlFor="agree" className="text-sm">
+            Agree with terms
+          </label>
         </div>
       </div>
-      <button>Register</button>
+      <button
+        type="submit"
+        className="bg-slate-600 hover:bg-slate-700 font-medium text-sm py-2 px-4 rounded-lg text-white"
+      >
+        Register
+      </button>
     </form>
   );
 };
